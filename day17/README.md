b0b78595110b2c98d4f2a09b99315e45a4f74a5c
Peter 👍 Georgantas <peter@iheartjane.com>
2024-12-18 08:38:43 -0500

    day 17 part 2 wow

---
+++ day17/README.md
@@ -0,0 +1,46 @@
+# Day 17
+
+Part 1. This took time just to parse the directions. In the end it was pretty simple to implement.
+
+Part 2. Okay, this certainly takes things to a new level. First thought was to just try brute force it (ofc). I started at A: 0 and iterated. I created some optomization to exit early if the output began to diverge from the program. I left it running for the better part of a day. Nothing. Give up.
+
+Back the next day. It was clear I would need to reverse engineer the program to find a solution. I created this explaination:
+```
+2,4 -> B = A % 8           :: the first 3 bits
+1,1 -> B = B ^ 1           :: ensure the 1 bit -> 1,3,5,7
+7,5 -> C = A / 2pow(reg B) :: A / 2,8,32,128
+0,3 -> A = A / 8           :: shift A by 3 bits
+1,4 -> B = B ^ 4           :: 1,3,5,7 -> 5 7 1 3
+4,5 -> B = B ^ C           :: ???
+5,5 -> out  B % 8          :: the first 3 bits
+3,0 -> jump to start
+```
+First thing that jumps out is that 8 (3 bits) is important. I also see that the starting values for B and C don't matter. Next, I noticed that A has to be sufficiently large to be divided by 8 (shift 3 bits right) at last 15 times to produce enough output. So register A needs to start with a value consisting of 16 3 bit (0-7) "pieces".
+
+Great. I started with `1 << (3*15)` and tried brute forcing again (kidding).
+
+Playing around with different inputs, `2 << (3*15)`, `3 << (3*15)`, `4 << (3*15)`, ect, I saw I could change _just_ the last output. I started manually traversing the remaining "pieces":
+
+```
+//r = &registers{
+//	A: 5<<(3*15) ^ // 0
+//		6<<(3*14) ^ //3
+//		0<<(3*13) ^ //5
+//		0<<(3*12) ^ //5
+//		0<<(3*11) ^ //5
+//		0<<(3*10) ^ //4
+//		0<<(3*9) ^ //4
+//		0<<(3*8) ^ //1
+//		0<<(3*7) ^ //3
+//		0<<(3*6) ^ //0
+//		0<<(3*5) ^ //5
+//		0<<(3*4) ^ //7
+//		0<<(3*3) ^ //1
+//		0<<(3*2) ^ //1
+//		0<<(3*1) ^ //4
+//		0<<(3*0), //2
+//}
+```
+This exercise ended up being useful because I found that at "piece" 10, no value worked. I would need to "back up" and find the next value for "piece" 11, then iterrate "piece" 10 again.
+
+Finally, I implemented a loop with this logic.
--- day17/day17.go
+++ day17/day17.go
@@ -19,33 +19,28 @@ type registers struct {
 	C int
 }
 
-type instruction struct {
-	opcode  int
-	operand int
-}
-
-func (i instruction) run(r *registers) (*int, *int) {
-	switch i.opcode {
+func (r *registers) run(opcode, operand int) (*int, *int) {
+	switch opcode {
 	case 0:
 		//adv
 		denom := 1
-		for range i.comboOperand(r) {
+		for range r.comboOperand(operand) {
 			denom *= 2
 		}
 		r.A = r.A / denom
 		return nil, nil
 	case 1:
 		//bxl
-		r.B = r.B ^ i.operand
+		r.B = r.B ^ operand
 		return nil, nil
 	case 2:
 		//bst
-		r.B = i.comboOperand(r) % 8
+		r.B = r.comboOperand(operand) % 8
 		return nil, nil
 	case 3:
 		//jnz
 		if r.A != 0 {
-			return &i.operand, nil
+			return &operand, nil
 		}
 		return nil, nil
 	case 4:
@@ -54,12 +49,12 @@ func (i instruction) run(r *registers) (*int, *int) {
 		return nil, nil
 	case 5:
 		//out
-		out := i.comboOperand(r) % 8
+		out := r.comboOperand(operand) % 8
 		return nil, &out
 	case 6:
 		//bdv
 		denom := 1
-		for range i.comboOperand(r) {
+		for range r.comboOperand(operand) {
 			denom *= 2
 		}
 		r.B = r.A / denom
@@ -67,7 +62,7 @@ func (i instruction) run(r *registers) (*int, *int) {
 	case 7:
 		//cdv
 		denom := 1
-		for range i.comboOperand(r) {
+		for range r.comboOperand(operand) {
 			denom *= 2
 		}
 		r.C = r.A / denom
@@ -77,10 +72,10 @@ func (i instruction) run(r *registers) (*int, *int) {
 	}
 }
 
-func (i instruction) comboOperand(r *registers) int {
-	switch i.operand {
+func (r *registers) comboOperand(operand int) int {
+	switch operand {
 	case 0, 1, 2, 3:
-		return i.operand
+		return operand
 	case 4:
 		return r.A
 	case 5:
@@ -98,7 +93,7 @@ func main() {
 	splitinput := strings.Split(input, "\n\n")
 	registersInput := strings.Split(splitinput[0], "\n")
 
-	r := registers{}
+	r := &registers{}
 	r.A, _ = strconv.Atoi(strings.Split(registersInput[0], ": ")[1])
 	r.B, _ = strconv.Atoi(strings.Split(registersInput[1], ": ")[1])
 	r.C, _ = strconv.Atoi(strings.Split(registersInput[2], ": ")[1])
@@ -109,6 +104,11 @@ func main() {
 		program[i], _ = strconv.Atoi(programInput[i])
 	}
 
+	part1(r, program)
+	part2(program)
+}
+
+func run(r *registers, program []int) []int {
 	instructionPointer := 0
 	var allOut []int
 	for {
@@ -117,7 +117,7 @@ func main() {
 		}
 		opcode := program[instructionPointer]
 		operand := program[instructionPointer+1]
-		jump, out := instruction{opcode, operand}.run(&r)
+		jump, out := r.run(opcode, operand)
 		if jump == nil {
 			instructionPointer += 2
 		} else {
@@ -127,11 +127,143 @@ func main() {
 			allOut = append(allOut, *out)
 		}
 	}
+	return allOut
+}
 
-	allOutS := make([]string, len(allOut))
-	for i := range allOut {
-		allOutS[i] = strconv.Itoa(allOut[i])
+func part1(r *registers, program []int) {
+	allOut := run(r, program)
+	printProgram(allOut)
+}
+
+func part2(program []int) {
+	printProgram(program)
+	var r *registers
+
+	m := make(map[int]int)
+	j := 0
+	for i := 15; i >= 0; i-- {
+		for ; j <= 7; j++ {
+			//fmt.Println(i, j)
+			regA := 0
+			regA = regA ^ (j << (3 * i))
+			for k, v := range m {
+				regA = regA ^ (v << (3 * k))
+			}
+			if regA == 0 {
+				continue
+			}
+
+			r = &registers{
+				A: regA,
+			}
+
+			out := run(r, program)
+			if out[i] == program[i] {
+				m[i] = j
+				break
+			}
+
+			//r = &registers{
+			//	A: 5<<(3*15) ^ // 0
+			//		6<<(3*14) ^ //3
+			//		0<<(3*13) ^ //5
+			//		0<<(3*12) ^ //5
+			//		0<<(3*11) ^ //5
+			//		0<<(3*10) ^ //4
+			//		0<<(3*9) ^ //4
+			//		0<<(3*8) ^ //1
+			//		0<<(3*7) ^ //3
+			//		0<<(3*6) ^ //0
+			//		0<<(3*5) ^ //5
+			//		0<<(3*4) ^ //7
+			//		0<<(3*3) ^ //1
+			//		0<<(3*2) ^ //1
+			//		0<<(3*1) ^ //4
+			//		0<<(3*0), //2
+			//}
+		}
+		if _, ok := m[i]; !ok {
+			j = m[i+1] + 1
+			delete(m, i+1)
+			i += 2
+		} else {
+			j = 0
+		}
+	}
+	regA := 0
+	for k, v := range m {
+		regA = regA ^ (v << (3 * k))
+	}
+	fmt.Println(regA)
+	printProgram(run(&registers{A: regA}, program))
+}
+
+func programsEqual(p1, p2 []int) bool {
+	if len(p1) != len(p2) {
+		return false
+	}
+	for i, v := range p1 {
+		if v != p2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func printProgram(program []int) {
+	for i := range program {
+		fmt.Print(program[i])
+		if i == len(program)-1 {
+			fmt.Println()
+		} else {
+			fmt.Print(",")
+		}
+	}
+}
+
+func part2Brute(program []int) {
+	regA := 0
+regLoop:
+	for {
+		r := &registers{A: regA}
+		instructionPointer := 0
+		var allOut []int
+		for {
+			if instructionPointer >= len(program) {
+				break
+			}
+			opcode := program[instructionPointer]
+			operand := program[instructionPointer+1]
+			jump, out := r.run(opcode, operand)
+			if jump == nil {
+				instructionPointer += 2
+			} else {
+				instructionPointer = *jump
+			}
+			if out != nil {
+				allOut = append(allOut, *out)
+				if len(allOut) > len(program) || allOut[len(allOut)-1] != program[len(allOut)-1] {
+					if len(allOut) > 7 {
+						fmt.Println(regA, allOut[:8])
+					}
+					break
+				}
+			}
+		}
+
+		if len(allOut) != len(program) {
+			regA++
+			continue regLoop
+		}
+		for i, v := range program {
+			if v != allOut[i] {
+				regA++
+				continue regLoop
+			}
+		}
+
+		break
 	}
 
-	fmt.Println(strings.Join(allOutS, ","))
+	fmt.Println("part2", regA)
 }
