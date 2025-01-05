fbf53517f50cf3d33619e135c895e49e5bef7f9b
Peter 👍 Georgantas <peter@iheartjane.com>
2025-01-05 18:46:54 -0500

    day 22 part 2

---
+++ day22/README.md
@@ -0,0 +1,5 @@
+# Day 22
+
+The most difficult part here was interpreting the directions. The order of operations was pretty confusing to the point where I thought this was going to be impossible. Coming back to it a few days later it wasn't too bad at all.
+
+Part1 and Part2 didn't require any real optimizations just iterate and compute.
--- day22/day22.go
+++ day22/day22.go
@@ -10,6 +10,9 @@ import (
 //go:embed input
 var input string
 
+//go:embed inputtest
+var inputtest string
+
 type secretNumber int
 
 func (sn secretNumber) mix(in int) secretNumber {
@@ -32,20 +35,55 @@ func (sn secretNumber) next() secretNumber {
 	return r
 }
 
+func (sn secretNumber) cost() int {
+	return int(sn) % 10
+}
+
 func main() {
 
 	buyersInput := strings.Split(input, "\n")
 
+	// changes to buyer num to banana count
+	changes := map[[4]int]map[int]int{}
+
 	total := 0
-	for _, buyerInput := range buyersInput {
+	for buyerNum, buyerInput := range buyersInput {
 		buyerInitial, _ := strconv.Atoi(buyerInput)
 		buyerSn := secretNumber(buyerInitial)
-		fmt.Println(buyerSn)
-		for range 2000 {
+		prevCost := buyerSn.cost()
+		prevChanges := [4]int{}
+		for i := range 2000 {
 			buyerSn = buyerSn.next()
+			currCost := buyerSn.cost()
+			currChanges := [4]int{prevChanges[1], prevChanges[2], prevChanges[3], currCost - prevCost}
+			if i >= 3 {
+				if m, ok := changes[currChanges]; ok {
+					if _, ok2 := m[buyerNum]; !ok2 {
+						m[buyerNum] = currCost
+					}
+				} else {
+					changes[currChanges] = map[int]int{buyerNum: currCost}
+				}
+
+			}
+			prevCost = buyerSn.cost()
+			prevChanges = currChanges
 		}
 		total += int(buyerSn)
 	}
 
 	fmt.Println("part1", total)
+
+	mostBananas := 0
+	for _, v := range changes {
+		changesTotal := 0
+		for _, b := range v {
+			changesTotal += b
+		}
+		if changesTotal > mostBananas {
+			mostBananas = changesTotal
+		}
+	}
+
+	fmt.Println("part2", mostBananas)
 }
